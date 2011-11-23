3b41cd39e9bc1f3278c949f8778b94b223bee26f
Mathias Buus <mathiasbuus@gmail.com>
2011-11-23 23:55:01 +0100

    better ssl support

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var delimited = require('./delimited');
 var websock = require('websock');
 var common = require('common');
 var http = require('http');
+var https = require('https');
 var parseURL = require('url').parse;
 
 var noop = function() {};
@@ -15,6 +16,8 @@ var JSONSocket = common.emitter(function(connection, open) {
 
 	this._buffer = [];
 	this._ping = null;
+	this._pong = null;
+	this._lastPong = Date.now();
 
 	if (open) {
 		this.send = this._send;
@@ -36,6 +39,7 @@ var JSONSocket = common.emitter(function(connection, open) {
 			return;
 		}
 		if (message === 'pong') {
+			self._lastPong = Date.now();
 			return;
 		}
 		try {
@@ -71,7 +75,19 @@ JSONSocket.prototype.end = function() {
 	this.connection.end();	
 };
 JSONSocket.prototype.ping = function() {
-	this._ping = setInterval(this.connection.send.bind(this.connection, 'ping'), 60*1000);
+	var self = this;
+	var ping = function() {
+		var now = Date.now();
+
+		if ((now-self._lastPong) > 2*60*1000) {
+			self.destroy();
+			return;
+		}
+		self.connection.send('ping');		
+
+	};
+
+	this._ping = setInterval(ping, 60*1000);
 };
 
 JSONSocket.prototype._send = function(message) {
@@ -90,13 +106,13 @@ exports.connect = function(host) {
 	}
 	host = parseURL(host);
 
-	if (host.protocol === 'ws:') {
+	if (host.protocol === 'ws:' || host.protocol === 'wss:') {
 		return new JSONSocket(websock.connect(host), false);
 	}
 
 	var transport = delimited.create();
 	var socket = new JSONSocket(transport, false);
-	var req = http.request({
+	var req = (host.protocol === 'jsons:' ? https : http).request({
 		agent: false,
 		port: host.port,
 		host: host.hostname,
